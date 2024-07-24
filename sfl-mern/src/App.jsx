b58bc688fa27c4b389cb7956ed2a2f4c1c30fb72
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-07-24 21:57:47 +0530

    integrated auth0

--- sfl-mern/src/App.jsx
+++ sfl-mern/src/App.jsx
@@ -1,16 +1,30 @@
+import React from 'react';
 import './App.css';
-import Header from "./components/header"
-import Home from "./pages/Home"
-import Footer from "./components/footer"
-
+import Header from "./components/header";
+import Home from "./pages/Home";
+import Footer from "./components/footer";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import history from './api/history';
 function App() {
+  const { isLoading, error } = useAuth0();
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
 
   return (
+    <Router history={history}>
     <>
-    <Header></Header>
-    <Home></Home>
-    <Footer></Footer>
+      <Header />
+      <Home />
+      <Footer />
     </>
+    </Router>
   );
 }
 
--- sfl-mern/src/pages/Home.jsx
+++ sfl-mern/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import "../css/Home.css"
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react';
+import "../css/Home.css";
+import { useAuth0 } from "@auth0/auth0-react";
+
 function Home() {
-    return (
-      <div className="Home">
-        <h1 className="home-logo">MERN-AUTH0</h1>
-        <h3 className="home-login-but">Login</h3>
-      </div>
-    );
-  }
-  
-  export default Home;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="Home">
+      <h1 className="home-logo">MERN-AUTH0</h1>
+      <h3 className="home-login-but" onClick={() => loginWithRedirect()}>Login</h3>
+    </div>
+  );
+}
+
+export default Home;
+
